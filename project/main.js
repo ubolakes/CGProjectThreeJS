6c2a6e68430d2b452ba45204fcd143c219dca00c
Umberto Laghi <ul2509@gmail.com>
2024-06-30 16:45:35 +0200

    removed useless imports

--- project/main.js
+++ project/main.js
@@ -4,13 +4,8 @@ E-mail: umberto.laghi@studio.unibo.it
 Github: @ubolakes
 */
 
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 // importing home made libraries
-import { Box, boxCollision, fallOff } from './resources/box.js';
-import * as UTILS from './resources/utils.js';
 import * as SCENE from './scene/scene.js';
-
 
 // taking canvas from index.html
 const canvas = document.getElementById("canvas");
